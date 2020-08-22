e165a2049ae41fa6d451ad7e22bcf31e0348af9b
Max Wheeler <maxwhee@microsoft.com>
2020-08-22 16:15:05 -0700

    use env for PORT

--- server.js
+++ server.js
@@ -39,7 +39,8 @@ function start(){
 	requestHandlers.setupHandlers(app);
 
 
-	const port = 8080;
+	//const port = 8080;
+	const port = process.env.PORT
 	app.listen(port, () => console.log(`Listening on port ${port}!`));
 
 
